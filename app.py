3f87b39e6ccbb956ac2a9d74e5ff508a48c56b45
Joeldev89 <joelorellana989@gmail.com>
2023-03-30 22:55:11 -0600

    Adding first version of most common words

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 # import plotly.express as px
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+
 st.title("Internal Testing Deployment Model")
 st.sidebar.title("Deep Dive analysis with NLP and GPT")
 st.markdown("This application is a Streamlit dashboard used "
@@ -31,4 +33,18 @@ st.header('Summary statistics')
 st.write("Description of the dataframe")
 st.write(df.describe())
 st.write("Most common words")
+st.sidebar.header("Word Cloud")
+word_sentiment = st.sidebar.radio('Display word cloud for what kind of reviews?', ('POS', 'NEU', 'NEG'))
+if not st.sidebar.checkbox("Close", True, key='3'):
+    st.subheader('Word cloud for %s reviews' % (word_sentiment))
+    df = df[df['sent_per_review'] == word_sentiment]
+    # st.write(df)
+    reviews = df.comments.values
+    my_wordcloud = WordCloud(stopwords=STOPWORDS, background_color='white', width=800, height=640, min_word_length=4, max_words=25).generate(str(reviews))
+    plt.imshow(my_wordcloud)
+    plt.xticks([])
+    plt.yticks([])
+    st.pyplot()
+
+
 st.header('Sentiment Analysis')
